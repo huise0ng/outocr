077979fe901b25f1e575b39988989fc2bc77938b
huise0ng <shinhuiseong07@gmail.com>
2024-07-23 15:40:33 +0900

    ADD :: 더 많은 코드리뷰 가능

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,36 +1,44 @@
 module.exports = {
-    parser: '@babel/eslint-parser', 
-    parserOptions: {
-      ecmaVersion: 2020,
-      sourceType: 'module',
-      requireConfigFile: false, 
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    requireConfigFile: false,
+  },
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
+  ],
+  plugins: ['react', 'react-hooks'],
+  rules: {
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': ['warn', { extensions: ['.jsx', '.js'] }],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    'no-console': 'warn',
+    'no-debugger': 'error',
+    'no-unescaped-entities': 'warn',
+    'no-unsafe-optional-chaining': 'error',
+    'react/jsx-uses-react': 'warn',
+    'react/jsx-uses-vars': 'warn',
+    'react/jsx-no-duplicate-props': 'error',
+    'react/no-deprecated': 'warn',
+    'react/no-direct-mutation-state': 'error',
+    'react/no-unknown-property': 'error',
+    'react/no-unused-state': 'warn',
+  },
+  settings: {
+    react: {
+      version: 'detect',
     },
-    env: {
-      browser: true,
-      es6: true,
-      node: true,
-    },
-    extends: [
-      'eslint:recommended',
-      'plugin:react/recommended',
-      'plugin:react-hooks/recommended',
-      'prettier', // Prettier와의 통합
-    ],
-    plugins: ['react', 'react-hooks'],
-    rules: {
-      'react/prop-types': 'off',
-      'react/react-in-jsx-scope': 'off',
-      'react/jsx-filename-extension': ['warn', { extensions: ['.jsx', '.js'] }],
-      'react-hooks/rules-of-hooks': 'error',
-      'react-hooks/exhaustive-deps': 'warn',
-      'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
-      'no-console': 'warn',
-      'no-debugger': 'error',
-    },
-    settings: {
-      react: {
-        version: 'detect',
-      },
-    },
-  };
-  
+  },
+};
